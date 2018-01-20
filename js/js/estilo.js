64618491c05820b9eb60d1ce5c3f1a1e4282666a
ema18787 <emanueljtc@gmail.com>
2018-01-19 21:10:49 -0400

    ciclos

--- js/js/estilo.js
+++ js/js/estilo.js
@@ -1,2 +1,48 @@
-var nombre = "Emanuel";
-alert(nombre)
+//VARIABLES
+/* var nombre;
+nombre = prompt("Ingresa Tu Nombre:", nombre);
+document.write(nombre);
+console.log(nombre); */
+
+//TIPOS DE DATOS
+
+// Numericos
+/* var numero = 12.3;
+alert("Esto es un dato numerico" + " " + numero);
+
+// String
+var texto = "BB TE AMO"
+alert("Esto es un texto" + " " + texto);
+
+// Array - Arreglo
+var arreglo = [1, 2, "Emanuel"];
+alert(arreglo[2]);
+
+//Objetos
+var objeto = {
+    color: "Black",
+    size: "23"
+}
+
+// Boolean - Boleano
+var ana = true;
+document.write(ana); */
+
+//ARREGLOS 
+
+//var amigo = prompt("Ingrese Nombre");
+//var amigos = ["Guille", "Luis", "Joaquin"];
+/* amigos[amigos.length] = amigo;
+console.log("Usted Tiene" + " " + amigos.length + " " + "amigos"); */
+//document.write(amigos.sort());
+
+//Condicionales
+
+var edad = prompt("Ingrese su Edad");
+
+if (edad >= 18) {
+    //Intrucciones
+    alert("Usted es Mayor de Edad, Pase");
+} else {
+    alert("Eres Menor de Edad, NO PASE");
+}
