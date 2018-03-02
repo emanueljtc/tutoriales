41f4aedd3180a924d6ea52882766ac3260dc2315
Emanueljtc <emanueljtc@gmail.com>
2018-03-02 11:13:15 -0400

    conectando con firebase

--- vuelinksapp/src/App.vue
+++ vuelinksapp/src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app" class="container">
+    <h1>Vue and Firebase</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+//importamos Firebase
+import Firebase from 'firebase';
+
+//creamos la variable que contiene los datos de la APIKEY
+let config = {
+  apiKey: "AIzaSyDK5LixDK_ZbPEFtTbrnRATXyYjVSBnn20",
+    authDomain: "vuefire-906a9.firebaseapp.com",
+    databaseURL: "https://vuefire-906a9.firebaseio.com",
+    projectId: "vuefire-906a9",
+    storageBucket: "",
+    messagingSenderId: "326103442942"
+};
+
+//Firebase conectate
+let app = Firebase.initializeApp(config);
+
+//Firebase trae la Base de Datos
+let db = app.database();
+
+let linksRef = db.ref('links')
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
--- vuelinksapp/src/main.js
+++ vuelinksapp/src/main.js
@@ -2,12 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// firebase
+import VueFire from 'vuefire';
 
 Vue.config.productionTip = false
 
+Vue.use(VueFire);
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    components: { App },
+    template: '<App/>'
 })
