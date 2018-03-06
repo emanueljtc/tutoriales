9ca32471f86b21e604a2be87e749d3796733dd20
Emanueljtc <emanueljtc@gmail.com>
2018-03-06 18:41:02 -0400

    componentes modulos y servicios

--- angular-crud-firebase/src/app/app.module.ts
+++ angular-crud-firebase/src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import  { AngularFireDatabaseModule  } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+//components
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-crud-firebase/src/environments/environment.ts
+++ angular-crud-firebase/src/environments/environment.ts
@@ -3,6 +3,12 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment = {
-  production: false
-};
+export const environment = { production: false, 
+  firebase: { 
+    apiKey: "AIzaSyA20lcAq86ra2p-3ZpGN7IlC_L4V1MIVQI", 
+    authDomain: "angular-crud-firebase-f3503.firebaseapp.com", 
+    databaseURL: "https://angular-crud-firebase-f3503.firebaseio.com", 
+    projectId: "angular-crud-firebase-f3503", 
+    storageBucket: "angular-crud-firebase-f3503.appspot.com", 
+    messagingSenderId: "685982562318" } 
+  };
