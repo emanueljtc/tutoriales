4677ea73272334f823e15db0ee92cfcc0e8e18fc
Emanueljtc <emanueljtc@gmail.com>
2018-03-07 16:45:39 -0400

    task

--- tasks-app/src/app/app.module.ts
+++ tasks-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
@@ -8,7 +8,7 @@ import { TaskAddComponent } from './componentes/task-add/task-add.component';
 import { TasksListComponent } from './componentes/tasks-list/tasks-list.component';
 import { TaskComponent } from './componentes/task/task.component';
 
-
+import { DataService } from './services/data.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +18,12 @@ import { TaskComponent } from './componentes/task/task.component';
     TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    DataService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ tasks-app/src/app/services/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { Task } from '../models/task';
+@Injectable()
+export class DataService {
+  tasks: Task[];
+
+  constructor() { 
+    this.tasks = [];
+  }
+
+  getTask(): Task[]{
+    if(localStorage.getItem('task') == null){
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return this.tasks;
+  }
+
+  addTask(task: Task):void {
+    this.tasks.unshift(task);
+  }
+}
