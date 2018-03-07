ec6f80b12c66cf5541850e3dd5ea63b10078e805
Emanueljtc <emanueljtc@gmail.com>
2018-03-07 16:15:26 -0400

    tasks-list

--- tasks-app/src/app/app.module.ts
+++ tasks-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { TaskAddComponent } from './componentes/task-add/task-add.component';
+import { TasksListComponent } from './componentes/tasks-list/tasks-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TaskAddComponent
+    TaskAddComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
