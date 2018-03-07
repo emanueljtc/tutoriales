4c3a2c612f2c031310d4370c98dd18255cba411d
Emanueljtc <emanueljtc@gmail.com>
2018-03-07 16:07:53 -0400

    task

--- tasks-app/src/app/app.module.ts
+++ tasks-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { TaskAddComponent } from './componentes/task-add/task-add.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TaskAddComponent
   ],
   imports: [
     BrowserModule
