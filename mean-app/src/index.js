c0ed00c76ccf1942acc2d3cae81f4e56c4c9ebcf
Emanueljtc <emanueljtc@gmail.com>
2018-03-08 14:25:14 -0400

    estableciendo vista

--- mean-app/src/index.js
+++ mean-app/src/index.js
@@ -1,10 +1,12 @@
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const app = express();
 const indexRoutes = require("./routes/index");
 const tasksRoutes = require("./routes/tasks");
 
 // configuraciones
+app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 3000)
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
--- mean-app/src/routes/index.js
+++ mean-app/src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-    res.send('hola mundo');
+    res.render('index.html');
 });
 
 module.exports = router;
