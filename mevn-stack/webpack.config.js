48cf92c4503b67c0b5cf8f704e4ba4015ec05191
Emanueljtc <emanueljtc@gmail.com>
2018-03-10 00:16:57 -0400

    vue-loader

--- mevn-stack/webpack.config.js
+++ mevn-stack/webpack.config.js
@@ -1,8 +1,23 @@
 const path = require('path');
 module.exports = {
-    entry: './src/app/main.js',
+    entry: "./src/app/main.js",
     output: {
-        path: path.join(__dirname, 'src/public/js'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, "src/public/js"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'vue-loader'
+                }
+            }
+        ]
     }
-}
+};
