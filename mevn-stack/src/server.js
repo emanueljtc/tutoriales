99405d9bcf872a3f5eb50a840e3349a86b30fc2b
Emanueljtc <emanueljtc@gmail.com>
2018-03-09 22:32:40 -0400

    cambios

--- mevn-stack/src/server.js
+++ mevn-stack/src/server.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
@@ -8,8 +10,15 @@ mongoose.connect('mongodb://localhost/mevn-stack').then(() => console.log('base 
 app.set('port', process.env.PORT || 3000);
 
 //middlewares
+app.use(cors());
 app.use(bodyParser.json());
 
+//Routes - API
+
+
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
