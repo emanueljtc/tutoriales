c4052bf2bce162262b2bc317ebaccc8e28db0b19
Emanueljtc <emanueljtc@gmail.com>
2018-03-13 09:59:08 -0400

    cambios

---
+++ app-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h2>Hola soy Vue Js</h2>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test';
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app-vue/src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>SOY UN COMPONENTE</h2>
+</template>
+
+<script>
+    export default{
+        
+    }
+</script>
+
+<style media="screen">
+</style>
