62920c64991a47a1b20ebef7375600b9b2005b87
Emanueljtc <emanueljtc@gmail.com>
2018-03-13 12:43:32 -0400

    cambios

--- app-vue/src/components/Test.vue
+++ app-vue/src/components/Test.vue
@@ -1,10 +1,39 @@
 <template>
-    <h2>SOY UN COMPONENTE</h2>
+<div>
+  <div v-text="title"></div>
+    <div v-if="showName">
+        <p>{{user.firstName}}</p>
+    </div>
+    <div v-else>
+        <p>No puedo Mostrar Nombre</p>
+    </div>
+    <hr>
+    <ul>
+        <li v-for="task in tasks"
+            :key="task">
+            {{ task.title }}
+        </li>
+    </ul>
+</div>
 </template>
 
 <script>
     export default {
-    
+        data(){
+            return{
+                title: 'Soy un Componente',
+                user: {
+                    firstName: 'Emanuel',
+                    lastName: 'Torres'
+                },
+                myHtml: '<h3>Hola Vue</h3>',
+                showName: false,
+                tasks: [
+                    {title: 'comer'},
+                    {title: 'dormir'}
+                    ]
+            }
+        }
     }
 </script>
 
