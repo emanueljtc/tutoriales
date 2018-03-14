d4bd1bf7260640a5d747f9c6adf76066f23b6d58
Emanueljtc <emanueljtc@gmail.com>
2018-03-14 14:22:12 -0400

    sass

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,12 +1,10 @@
-"use strict";
-
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function() {
     return gulp
         .src("./sass/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
         .pipe(gulp.dest("./css"));
 });
 
