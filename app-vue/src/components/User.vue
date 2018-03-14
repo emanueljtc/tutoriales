439cfb32a111be013b1593a29f126165ec630405
Emanueljtc <emanueljtc@gmail.com>
2018-03-14 08:58:34 -0400

    cambios

--- app-vue/src/components/User.vue
+++ app-vue/src/components/User.vue
@@ -5,11 +5,31 @@
         <h4>Componente User</h4>
       </div>
       <div class="card-body">
-        <ul>
+          <table class="table table-hover table-striped">
+              <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Acciones</th>
+                </tr>                  
+              </thead>
+              <tbody>
+                  <tr v-for="user in users">
+                      <th scope="row">{{ user.id }}</th>
+                      <th>{{user.name}}</th>
+                      <th>{{user.email}}</th>
+                      <th>
+                          <i class="far fa-window-close"></i> </button>
+                      </th>
+                  </tr>
+              </tbody>
+          </table>
+        <!-- <ul>
           <li v-for="user in users">
             {{ user.name }} - {{ user.email }} <button v-on:click="deleteUser(user)"> <i class="far fa-window-close"></i> </button>
           </li>
-        </ul>
+        </ul> -->
         <form v-on:submit.prevent="addUser">
           <div class="row">
 
@@ -34,13 +54,7 @@
   export default {
     data() {
       return {
-        users: [{
-            name: 'Emanuel Torres',
-            email: 'emanueljtc@gmail.com',
-            contacted: false
-          }
-          
-        ],
+        users: [],
         newUser: {}
       }
     },
@@ -52,6 +66,10 @@
       deleteUser(user){
          this.users.splice(this.users.indexOf(user), 1);
       }
+    },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => this.users = res.body);
     }
   }
 
--- app-vue/src/main.js
+++ app-vue/src/main.js
@@ -1,13 +1,35 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App";
+import Test from "./components/Test.vue";
+import User from "./components/User.vue";
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
+Vue.use(VueResource);
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    {
+      path: "/",
+      component: User
+    },
+    {
+      path: "/",
+      component: Test
+    }
+  ]
+});
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  el: "#app",
+  components: {
+    App
+  },
+  router,
+  template: "<App/>"
+});
