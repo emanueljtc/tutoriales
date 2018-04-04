5d2f8ef5ffc9c55a742f8818cd62f9ac82f9082b
Emanueljtc <emanueljtc@gmail.com>
2018-04-04 18:06:19 -0400

    componente convertidor

---
+++ vue_curso/05WebApps/SPA/src/components/Currency.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+	<h1>{{ title }}</h1>
+	<img src="../assets/logo.png" alt="Vue.js">
+
+</div>
+</template>
+<script>
+	export default{
+		name:"currency",
+		data(){
+			return {
+				title:'Convertidor de Divisas'
+			}
+		}
+	};
+</script>
