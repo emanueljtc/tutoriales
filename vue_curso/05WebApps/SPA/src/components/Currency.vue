a9054a3f8c9f1ef7b60f72a5b9bafb7884835c64
Emanueljtc <emanueljtc@gmail.com>
2018-04-05 19:11:48 -0400

    componente

--- vue_curso/05WebApps/SPA/src/components/Currency.vue
+++ vue_curso/05WebApps/SPA/src/components/Currency.vue
@@ -2,15 +2,75 @@
 <div>
 	<h1>{{ title }}</h1>
 	<img src="../assets/logo.png" alt="Vue.js">
-
+	<div class="row">
+		<div class="col s4 offset-s4">
+			<label class="flow-text">DE</label>
+			<currency-rates-list 
+				:list="baseList()"
+				:initialValue="base" 
+				name="base"
+			>
+				
+			</currency-rates-list>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s4 offset-s4">
+			<label class="flow-text">A</label>
+			<currency-rates-list></currency-rates-list>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s4 offset-s4">
+			<label class="flow-text">MONTO</label>
+			<input type="number" min="1" class="center">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s12">
+			<h5>Valor de Conversión</h5>
+		</div>
+	</div>
 </div>
 </template>
 <script>
+import {getRates, convertRates} from '../helpers/api_fixer'
+import CurrencyRatesList from './CurrencyRatesList'
 	export default{
 		name:"currency",
+		components:{
+			CurrencyRatesList
+		},
 		data(){
 			return {
-				title:'Convertidor de Divisas'
+				title:'Convertidor de Divisas',
+				currencyList:[],
+				base:'',
+				target:'',
+				base_amount: 1,
+				target_amount:1
+			}
+		},
+		created(){
+			getRates('USD').then(res => {
+				//console.log(res)
+				this.currencyList = res
+				this.base = res[0]
+				this.target = res[1]
+			});
+		},
+		methods:{
+			changeRates(){
+
+			},
+			convertRates(){
+
+			},
+			baseList(){
+
+			},
+			targetList(){
+
 			}
 		}
 	};
