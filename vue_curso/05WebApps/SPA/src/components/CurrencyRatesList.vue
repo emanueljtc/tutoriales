d46cc857681dcd8f8e39e26f0ded5f90040ec2f8
Emanueljtc <emanueljtc@gmail.com>
2018-04-05 19:53:08 -0400

    hijo

--- vue_curso/05WebApps/SPA/src/components/CurrencyRatesList.vue
+++ vue_curso/05WebApps/SPA/src/components/CurrencyRatesList.vue
@@ -1,33 +1,32 @@
 <template>
-<select class="browser-default" :name="name" v-model="value">
-	<option :value="currency" v-for="(currency, key) in list" :key="key">
-		{{ currency.name }}
-	</option>
-</select>
+  <select class="browser-default" :name="name" v-model="value">
+    <option :value="currency" v-for="(currency, key) in list" :key="key">
+      {{ currency.name }}
+    </option>
+  </select>
 </template>
 <script>
-	export default{
-		name:"currency-rates-list",
-		data(){
-			return{
-				value:{}
-			}
-		},
-
-		watch:{
-			initialValue(){
-
-			},
-			value(){
-
-			}
-		},
-		methods:{
-			changeRates(value){
-
-			}
-		},
-		props:['list','initialValue','name']
-
-	};
+export default {
+  name: "currency-rates-list",
+  data() {
+    return {
+      value: {}
+    };
+  },
+  //https://vuejs.org/v2/guide/computed.html#Watchers
+  watch: {
+    initialValue(val, oldVal) {
+      this.value = val;
+    },
+    value(val, oldVal) {
+      this.changeRates(val);
+    }
+  },
+  methods: {
+    changeRates(val) {
+      this.$emit("changeRates", val, this.name);
+    }
+  },
+  props: ["list", "initialValue", "name"]
+};
 </script>
