7477cee902f98282a654d3c635ea435dff507ddd
Emanueljtc <emanueljtc@gmail.com>
2018-04-05 19:11:54 -0400

    currency

---
+++ vue_curso/05WebApps/SPA/src/components/CurrencyRatesList.vue
@@ -0,0 +1,33 @@
+<template>
+<select class="browser-default" :name="name" v-model="value">
+	<option :value="currency" v-for="(currency, key) in list" :key="key">
+		{{ currency.name }}
+	</option>
+</select>
+</template>
+<script>
+	export default{
+		name:"currency-rates-list",
+		data(){
+			return{
+				value:{}
+			}
+		},
+
+		watch:{
+			initialValue(){
+
+			},
+			value(){
+
+			}
+		},
+		methods:{
+			changeRates(value){
+
+			}
+		},
+		props:['list','initialValue','name']
+
+	};
+</script>
