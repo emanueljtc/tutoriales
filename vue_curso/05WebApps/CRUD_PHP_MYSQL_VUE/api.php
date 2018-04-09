b30617ed230be5769b769c38ccfa1e416a9f4b72
Emanueljtc <emanueljtc@gmail.com>
2018-04-09 08:03:22 -0400

    condicion read y create de API

--- vue_curso/05WebApps/CRUD_PHP_MYSQL_VUE/api.php
+++ vue_curso/05WebApps/CRUD_PHP_MYSQL_VUE/api.php
@@ -4,3 +4,37 @@ $conn = new mysqli('localhost','root','ema18787','vue_students');
 if($conn->connect_error){
 	die('Error al conectarse a la base de datos');
 }
+$res = array('error'=>false);
+
+$action = 'read';
+
+if(isset($_GET['action'])){
+	$action = $_GET['action'];
+}
+
+if ($action == 'read'){
+	$result = $conn->query("SELECT * FROM students");
+	$studens = array();
+
+	while($row = $result->fetch_assoc()){
+		array_push($students, $row);
+	}
+
+	$res['students']=$students;
+}
+
+if( $action == 'create'){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$web = $_POST['web'];
+
+	$result = $conn->query("INSERT INTO students(name, email,web) 
+	VALUES('$name','$email','$web')");
+
+	if($result){
+		$res['message']='Estudiante agregado con éxito.';
+	}else{
+		$res['error']= true;
+		$res['message']="Error al tratar de registrar el estudiante."; 
+	}
+}
