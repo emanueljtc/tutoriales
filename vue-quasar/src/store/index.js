1ccfc0f7de76ff6363c6597a5a20bc18da011be6
Emanueljtc <emanueljtc@gmail.com>
2018-04-11 11:39:02 -0400

    cambios

--- vue-quasar/src/store/index.js
+++ vue-quasar/src/store/index.js
@@ -1,14 +1,15 @@
+import "../helpers/firebase";
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import VueFire from "vuefire";
 import example from './module-example'
-
+Vue.use(VueFire);
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  modules: {
-    example
-  }
+    modules: {
+        example
+    }
 })
 
 export default store
