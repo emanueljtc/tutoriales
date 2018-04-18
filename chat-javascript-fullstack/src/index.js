196c8fc9af2616a39d7edae3cf02060c59ed3185
Emanueljtc <emanueljtc@gmail.com>
2018-04-18 10:33:07 -0400

    puerto automatico o por  defecto

--- chat-javascript-fullstack/src/index.js
+++ chat-javascript-fullstack/src/index.js
@@ -8,12 +8,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio.listen(server);
 
+//Configuracion de puerto automatico o por defecto
+app.set('port', process.env.PORT || 3000)
+
+//Web Sockets
 require('./sockets')(io);
 
 //Archivos Estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Iniciando el servidor
-server.listen(3000, () => {
-    console.log('Servidor en el puerto 3000');
+server.listen(app.get('port'), () => {
+    console.log('Servidor en el puerto ', app.get('port'));
 });
