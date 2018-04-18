3d0c6e8f147a9f588730174be994eabd3e4bd33d
Emanueljtc <emanueljtc@gmail.com>
2018-04-18 10:04:43 -0400

    conectando con socket

--- chat-javascript-fullstack/index.js
+++ chat-javascript-fullstack/index.js
@@ -5,10 +5,17 @@ const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-socketio.listen(server);
+const io = socketio.listen(server);
 
+io.on('connection', socket => {
+    console.log('Nuevo Usuario Conectado');
+})
+
+
+//Archivos Estaticos
 app.use(express.static('public'));
 
+//Iniciando el servidor
 server.listen(3000, () => {
     console.log('Servidor en el puerto 3000');
 });
