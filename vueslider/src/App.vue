f3ee15023a72be3b1ff73aff3ed997c41d542b19
Emanueljtc <emanueljtc@gmail.com>
2018-04-19 16:22:28 -0400

    components slides

--- vueslider/src/App.vue
+++ vueslider/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Slides></Slides>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Slides from './components/Slides'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slides
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 900px;
 }
 </style>
