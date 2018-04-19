154b450f73ef953419471ea82701827aba77d3f4
Emanueljtc <emanueljtc@gmail.com>
2018-04-19 17:58:41 -0400

    cambios no sirvio

---
+++ vueslider/src/components/Slide.vue
@@ -0,0 +1,23 @@
+<template>
+		<div class="slide">
+			<img v-bind:src="slide.src" alt="">
+		</div>
+</template>
+<script>
+	export default{
+		data() {
+			return{}
+		},
+		props:
+			["slide"]
+	
+	}
+</script>
+<style scoped>
+img{
+	max-width: 100%;
+}
+.slide{
+	display: inline-block
+}
+</style>
--- vueslider/src/components/Slides.vue
+++ vueslider/src/components/Slides.vue
@@ -1,14 +1,13 @@
 <template>
-	<div class="slides">
-		<div class="slides-inner">
-			<div v-for="slide in slides" :key="slide">
-				{{slide.src}}
-			</div>
-		</div>
+<div class="slides">
+	<div :style="{ width: innerWidth+ 'px'}" class="slides-inner">
+		<Slide v-for="slide in slides" :style="{ width: singleWidth+'px'}" :slide="slide" :key="slide.key" ></Slide>
 	</div>
+</div>
 </template>
 
 <script>
+	import Slide from './Slide';
 	export default {
 		data(){
 			return{
@@ -19,8 +18,26 @@
 					{src: 'static/img/slide4.jpeg'},
 					{src: 'static/img/slide5.jpeg'}
 																				
-					]
+					],
+					innerWidth: 0,
+					singleWidth: 0
+
 			}
+		},
+		props: {
+			itemsPerSlide:{
+				type: null,
+				default: 1
+			}
+		},
+		components: {
+			Slide
+
+		},
+		attached(){
+			let singleWidth = this.$el.clientWith/this.itemsPerSlide
+			this.$set('singleWidth', singleWidth)
+			this.$set('innerWidth', singleWidth * this.slides.length)
 		}
 	}
 </script>
