62a388571f64282ee99eca4d69c76377e4463dc5
Emanueljtc <emanueljtc@gmail.com>
2018-04-19 16:22:37 -0400

    agragando rutas data

--- vueslider/src/components/Slides.vue
+++ vueslider/src/components/Slides.vue
@@ -1,14 +1,30 @@
 <template>
 	<div class="slides">
-			Slides va a ir aqui
-	</div>	
+		<div class="slides-inner">
+			<div v-for="slide in slides" :key="slide">
+				{{slide.src}}
+			</div>
+		</div>
+	</div>
 </template>
+
 <script>
 	export default {
-		
+		data(){
+			return{
+				slides: [
+					{src: 'static/img/slide1.jpeg'},
+					{src: 'static/img/slide2.jpeg'},
+					{src: 'static/img/slide3.jpeg'},
+					{src: 'static/img/slide4.jpeg'},
+					{src: 'static/img/slide5.jpeg'}
+																				
+					]
+			}
+		}
 	}
 </script>
+
 <style scoped>
-
+	
 </style>
-
