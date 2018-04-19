4361cf08c73234a7d571477713885ddd73e2b00e
Emanueljtc <emanueljtc@gmail.com>
2018-04-19 11:21:01 -0400

    definiendo el schema

---
+++ chat-javascript-fullstack/src/models/Chat.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ChatSchema = new Schema({
+    nick: String,
+    msg: String,
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
