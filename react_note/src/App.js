04e49711f0b9c622f080d1fc4d5a4ae278a837c4
emanueljtc <emanueljtc@gmail.com>
2018-05-29 10:52:05 -0400

    new app react

---
+++ react_note/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="notesContainer">
+          <div className="notesHeader">
+             <h1>React_Notes</h1>
+          </div>
+          <div classNote="notesBody">
+
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
