037c0dfa71d7fbe4485a1e164048c9a13044c681
emanueljtc <emanueljtc@gmail.com>
2018-05-29 08:27:03 -0400

    cambiando

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -4,21 +4,14 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h1 className = "App-title" > Bienvenido a React < /h1> <
-            /header> <
-            p className = "App-intro" >
-            To get started, edit < code > src / App.js < /code> and save to reload. <
-            /p> <
-            /div>
+        return ( <div className = "App">
+            <header className = "App-header">
+            <img src = { logo } className = "App-logo" alt = "logo" />
+            <h1 className = "App-title"> Bienvenido a React </h1>
+            </header>
+            <p className = "App-intro">
+            Empieza a editar <code> src/App.js </code> y recarga. </p>
+            </div>
         );
     }
 }
