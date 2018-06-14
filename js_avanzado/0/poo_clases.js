27675427843d78940f5b467a3fee49b03114aa25
ema18787 <emanueljtc@gmail.com>
2018-06-14 17:34:20 -0400

    poo con clases

---
+++ js_avanzado/0/poo_clases.js
@@ -0,0 +1,82 @@
+((c) => {
+    //POO con Clases
+    /* Con la llegada de ES6 la definicion de una funcion ha cambiado y nos
+    ofrece la posibilidad de crear clases 
+    
+    ES6 aporta un azucar sintactico para declarar un clase como en la mayoria
+    de los lenguajes POO, pero por debajo sigue siendo una funcion prototipal
+    
+    El metodo especial constuctor recibe los parametros que anteriormente recibia la 
+    funcion constructora
+    
+    Las Clases ES6 sustituyen a las funciones prototipales de ES5
+
+    Las declaraciones de clases no siguen las reglas de hoisting como si lo hacen
+    las declaraciones de funciones, esto quiere decir que solo existen tras ser declaradas
+    
+    De forma implicita una clase se comporta como una constante, no siendo posible redeclararla
+    mas adelante en un mismo ambito o scope
+
+    los metodos no se declaran de forma explicita como var, let o const
+
+    Al tratarse de un constructor y no una funcion, no hay una salida de datos
+    explicita con return
+
+    Encontramos nuevas palabras reservadas: constructor, super, get, set, static
+    */
+    c('***** POO con Clases *****')
+
+    class Carrito {
+
+        constructor(articulo) {
+            this._articulo = articulo,
+                this._carrito = {}
+        }
+        agregar(articulo, cantidad) {
+            this._carrito[articulo] = cantidad
+        }
+        quitar(articulo) {
+            delete this._carrito[articulo]
+        }
+        _iterable() {
+            let message = 'Carrito: \n'
+            for (let key in this._carrito)
+                message += `\t${this._carrito[key]} ${key}\n`
+
+            return message
+        }
+        ver(articulo = 'todos') {
+            return (articulo === 'todos') ?
+                this._iterable() :
+                (this._carrito.hasOwnProperty(articulo)) ?
+                `${this._carrito[articulo]} ${articulo}` :
+                `El articulo ${articulo} no existe en el Carrito`
+        }
+    }
+    const comics = new Carrito('Comics')
+    c(comics)
+    comics.agregar('Flash Point Paradox', 2)
+    comics.agregar('The Return of the Dark Knight', 3)
+    comics.agregar('Civil War', 3)
+    comics.agregar('Final Crisis', 1)
+    c(comics.ver())
+    c(comics.ver('Flash int Paradox'))
+    c(comics.ver('Civil War'))
+    comics.quitar('Civi War')
+    c(comics.ver('Civi War'))
+    c(comics.ver())
+    const libros = new Carrito("Libros"),
+        musica = new Carrito("Música"),
+        juegos = new Carrito("Juegos"),
+        peliculas = new Carrito("Peliculas"),
+        series = new Carrito("Series")
+    c(
+        libros, '\n',
+        musica, '\n',
+        juegos, '\n',
+        peliculas, '\n',
+        series
+    )
+    libros.agregar('Santa Biblia', 1)
+    libros.ver()
+})(console.log);
--- js_avanzado/0/poo_prototipica.js
+++ js_avanzado/0/poo_prototipica.js
@@ -96,41 +96,37 @@
 
 })(console.log);
 ((c) => {
-    //POO con Constructor
-    c('*****POO con Constructor*****')
+    //POO con Funciones Constructoras
+    c('**********POO con Funciones Constructoras**********')
 
     function Carrito(articulo) {
-        this._articulo = articulo,
-            this._carrito = {}
+        this._articulo = articulo
+        this._carrito = {}
 
         this.agregar = (articulo, cantidad) => this._carrito[articulo] = cantidad
 
-        this.quitar = (articulo) => delete this._carrito[articulo]
+        this.quitar = articulo => delete this._carrito[articulo]
 
         this._iterable = () => {
             let message = 'Carrito: \n'
             for (let key in this._carrito)
-                message += `\t${this._carrito[key]} ${key}\n`
+                message += `\t${this. _carrito[key]} ${key}\n`
 
             return message
         }
 
         this.ver = (articulo = 'todos') => {
-            return (articulo === 'todos') ?
+            return (articulo === 'todos')
+                //? this._carrito
+                ?
                 this._iterable() :
                 (this._carrito.hasOwnProperty(articulo)) ?
                 `${this._carrito[articulo]} ${articulo}` :
                 `El articulo ${articulo} no existe en el Carrito`
         }
-
-        return {
-            agregar: agregar,
-            quitar: quitar,
-            ver: ver
-        }
     }
 
-    const comics = Carrito('Comics')
+    const comics = new Carrito('Comics')
     c(comics)
     comics.agregar('Flash Point Paradox', 2)
     comics.agregar('The Return of the Dark Knight', 3)
@@ -141,5 +137,133 @@
     c(comics.ver('Civil War'))
     comics.quitar('Civil War')
     c(comics.ver('Civil War'))
+    c(comics.ver())
+    c('**********Patrón de Diseño: Factoria o Fábrica**********')
+        /*
+          Esta forma de codificar las funciones como clases se conoce como Factory Pattern o Template functions
+          
+          Un problema importante que tiene este tipo de estructura, es que cuando creamos un nuevo objeto a partir de estas funciones, se reservará espacio en memoria para toda las funciones
+          
+          Con un objeto creado no supone mucha desventaja, pero con varios objetos sí
+        */
+    const libros = new Carrito("Libros"),
+        musica = new Carrito("Música"),
+        juegos = new Carrito("Juegos"),
+        peliculas = new Carrito("Peliculas"),
+        series = new Carrito("Series")
+
+    //Esto supone que los métodos agregar, quitar ver, e _iterable están siendo replicados en memoria, lo que es ineficiente
+    c(
+        libros, '\n',
+        musica, '\n',
+        juegos, '\n',
+        peliculas, '\n',
+        series
+    )
+
+    //Para solucionar esto podemos hacer uso del objeto Prototype que permite que objetos de la misma clase compartan métodos y no sean replicados en memoria de manera ineficiente
 
 })(console.log);
+((c) => {
+    //POO con Prototype
+    c('*****POO con Prototype*****')
+
+    function Carrito(articulo) {
+        this._articulo = articulo,
+            this._carrito = {}
+    }
+
+    Carrito.prototype = {
+        agregar: function(articulo, cantidad) {
+            this._carrito[articulo] = cantidad
+        },
+        quitar: function(articulo) {
+            delete this._carrito[articulo]
+        },
+        _iterable: function() {
+            let message = 'Carrito: \n'
+            for (let key in this._carrito)
+                message += `\t${this._carrito[key]} ${key}\n`
+
+            return message
+        },
+        ver: function(articulo = 'todos') {
+            return (articulo === 'todos') ?
+                this._iterable() :
+                (this._carrito.hasOwnProperty(articulo)) ?
+                `${this._carrito[articulo]} ${articulo}` :
+                `El articulo ${articulo} no existe en el Carrito`
+        }
+    }
+
+    /* Carrito.prototype.agregar = function () {}
+    Carrito.prototype.quitar = function () {}
+    Carrito.prototype.ver = function () {} */
+    const comics = new Carrito('Comics')
+    c(comics)
+    comics.agregar('Flash Point Paradox', 2)
+    comics.agregar('The Return of the Dark Knight', 3)
+    comics.agregar('Civil War', 3)
+    comics.agregar('Final Crisis', 1)
+    c(comics.ver())
+    c(comics.ver('Flash Point Paradox'))
+    c(comics.ver('Civil War'))
+    comics.quitar('Civil War')
+    c(comics.ver('Civil War'))
+    c(comics.ver())
+    const libros = new Carrito("Libros"),
+        musica = new Carrito("Música"),
+        juegos = new Carrito("Juegos"),
+        peliculas = new Carrito("Peliculas"),
+        series = new Carrito("Series")
+    c(
+        libros, '\n',
+        musica, '\n',
+        juegos, '\n',
+        peliculas, '\n',
+        series
+    )
+    libros.agregar('Santa Biblia', 1)
+    libros.ver()
+})(console.log);
+
+((c) => {
+    //Herencia Prototipica
+    // Las funciones Constructoras pueden heredar directamente de 
+    // constructores gracias al Prototype
+    c('****** Herencia Prototipica******')
+
+    function Telefono() {
+        this.puedoLlamar = true
+    }
+    Telefono.prototype = {
+        llamar: function() {
+            c('Riiing Riiing!!!!')
+        }
+    }
+
+    function Celular() {
+        this.tengoCables = false
+    }
+    Celular.prototype = new Telefono()
+    Celular.prototype.vibrar = function() {
+        c('Usted Recibio un Zumbido Vbrrrr Vbrrrrr!!!')
+    }
+
+    function Smartphone() {
+        this.tengoInternet = true
+    }
+    Smartphone.prototype = new Celular()
+    Smartphone.prototype.conectar = function() {
+        c('Conectado a Internet!!!')
+    }
+
+    let g4 = new Smartphone()
+    c(g4)
+    g4.llamar()
+    c(g4.puedoLlamar)
+    g4.vibrar()
+    c(g4.tengoCables)
+    g4.conectar()
+    c(g4.tengoInternet)
+})(console.log)
