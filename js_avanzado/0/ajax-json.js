af91236570650549e9516dac06f3f071cee8912e
ema18787 <emanueljtc@gmail.com>
2018-07-02 12:30:32 -0400

    cambios

--- js_avanzado/0/ajax-json.js
+++ js_avanzado/0/ajax-json.js
@@ -1,5 +1,18 @@
 ((c, d, ajax, j) => {
     const movies = d.querySelector('#movies')
-    let moviesInfo,
-        moviesTemplate = ''
+
+    // c('Json.stringify()', {}, j.stringify({}), 'Json.Parse', '{value: 2, result:2}', j.parse('{"value": "2", "result":"2"}'))
+    ajax.open('GET', './assets/movies.json', true)
+    ajax.addEventListener('load', e => {
+        let moviesInfo,
+            moviesTemplate = ''
+        if (ajax.status >= 200 && ajax < 400) {
+            moviesInfo = j.parse(ajax.responseText)
+            c(moviesInfo)
+
+        } else {
+
+        }
+    })
+    ajax.send()
 })(console.log, document, new XMLHttpRequest(), JSON);
