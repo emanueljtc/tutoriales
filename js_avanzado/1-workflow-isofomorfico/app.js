d2cfde6568e0c26c87c0fdab714b9b6081e53912
ema18787 <emanueljtc@gmail.com>
2018-07-11 12:49:31 -0400

    flujo isomorfico

---
+++ js_avanzado/1-workflow-isofomorfico/app.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import pug from 'pug'
+import favicon from 'serve-favicon'
+import sassMiddleware from 'node-sass-middleware'
+import routes from './src/app/routes'
+
+console.log('compila')
