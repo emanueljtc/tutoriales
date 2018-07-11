ec2b52bcc4232df3397c08b67696c48dafd44082
ema18787 <emanueljtc@gmail.com>
2018-07-11 14:59:28 -0400

    ack

--- js_avanzado/1-workflow-isofomorfico/app.js
+++ js_avanzado/1-workflow-isofomorfico/app.js
@@ -4,4 +4,41 @@ import favicon from 'serve-favicon'
 import sassMiddleware from 'node-sass-middleware'
 import routes from './src/app/routes'
 
+const env = 'development'
+port = process.env.PORT || 3000,
+    publicDir = `${__dirname}/public`,
+    viewDir = `${__dirname}/src/views`,
+    faviconDir = `${__dirname}/public/img/favicon.png`,
+    app = express()
+
+app
+    .set('views', viewDir)
+    .set('view engine', 'pug')
+    .set('port', port)
+    .set('env', env)
+
+.use(sassMiddleware({
+        src: `${__dirname}/src/scss`,
+        dest: publicDir,
+        debug: false,
+        outputStyle: 'compressed'
+    }))
+    .use(express.static(publicDir))
+    .use(favicon(faviconDir))
+    .use(routes)
+if (app.get('env') === 'production') {
+    app.use((req, res, next) => {
+        let err = new Error('Not Found')
+        err.status = 404
+        next(err)
+    })
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.render('error', { err: err })
+    })
+}
+
+export default app
+
+
 console.log('compila')
