bd3d3b6e836e73925a53bcaf5a89af0f35d006cc
ema18787 <emanueljtc@gmail.com>
2018-07-16 15:30:44 -0400

    stack MERN

---
+++ MERN-STACK-TASKS/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path')
+const app = express();
+// Settings
+app.set('port', process.env.PORT || 3000);
+// Midlewares
+app.use(morgan('dev'));
+app.use(express.json());
+// Routes
+app.use('/api/tasks', require('./routes/task.routes'));
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, 'public'))
+    // Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`)
+})
---
+++ MERN-STACK-TASKS/src/routes/task.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', (req, res) => {
+    res.json({
+        status: 'API Works!'
+    });
+});
+
+module.exports = router;
