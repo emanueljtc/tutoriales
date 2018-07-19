eafecf8a851a3dd770008e9e026d5be067ae5c5c
ema18787 <emanueljtc@gmail.com>
2018-07-19 10:50:08 -0400

    cambios

--- hello-world/src/components/App.js
+++ hello-world/src/components/App.js
@@ -8,14 +8,18 @@ import Footer from './Global/Footer';
 
 // Data
 import items from '../data/menu'
+//Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faStroopwafel)
 class App extends Component {
     render() {
         return (
           <div className = "App">
             <Header title="CodeJobs" items={items} />
             <Content />
-            <Footer />
+            <Footer copyright="&copy; Ing. Torres 2017" />
           </div>
         );
     }
