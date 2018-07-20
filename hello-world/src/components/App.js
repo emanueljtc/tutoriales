1b8b52319c221f39a268661b885aabeafd4b8986
ema18787 <emanueljtc@gmail.com>
2018-07-20 13:57:40 -0400

    cambios

--- hello-world/src/components/App.js
+++ hello-world/src/components/App.js
@@ -9,11 +9,6 @@ import Footer from './Global/Footer';
 
 // Data
 import items from '../data/menu'
-//Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faStroopwafel)
 class App extends Component {
   static PropTypes = {
     children: PropTypes.object.isRequired
--- hello-world/webpack.config.babel.js
+++ hello-world/webpack.config.babel.js
@@ -1,10 +1,6 @@
 // Dependencies
 import webpack from 'webpack';
 import path from 'path';
-import ChunksPlugin from 'webpack-split-chunks';
-
-// Environment
-const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // Paths
 const PATHS = {
@@ -12,70 +8,34 @@ const PATHS = {
   build: path.join(__dirname, 'src/public'),
   src: path.join(__dirname, 'src')
 };
-
-const getDevtool = () => 'cheap-module-eval-source-map';
-const getEntry = () => {
-  const entry = [PATHS.index];
-
-  if (isDevelopment) {
-    entry.push('webpack-hot-middleware/client?reload=true');
-  }
-
-  return entry;
-};
-const getOutput = () => ({
-  path: PATHS.build,
-  publicPath: '/',
-  filename: '[name].bundle.js'
-});
-const getPlugins = () => {
-  const plugins = [
-    new ChunksPlugin({
-      to: 'vendor',
-      test: /node_modules/
-    })
-  ];
-
-  if (isDevelopment) {
-    plugins.push(
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoEmitOnErrorsPlugin()
-    );
-  } else {
-    plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          screw_ie8: true,
-          warnings: false
-        }
-      })
-    );
-  }
-
-  return plugins;
-};
-const getLoaders = () => ({
-  loaders: [
-    {
-      test: /\.js?$/,
-      loaders: ['babel-loader'],
-      include: PATHS.src
-    },
-    {
-      test: /(\.css)$/,
-      loaders: ['style-loader', 'css-loader']
-    },
-    {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-    }
-  ]
-});
-
 export default {
-  devtool: getDevtool(),
-  entry: getEntry(),
-  output: getOutput(),
-  plugins: getPlugins(),
-  module: getLoaders()
+  devtool : 'cheap-module-eval-source-map',
+  entry : [
+    'webpack-hot-middleware/client?reload=true',
+    PATHS.index
+  ],
+  output: {
+    path: PATHS.build,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+      loaders: [{
+        test: /\.js?$/,
+        loaders: ['babel-loader'],
+        include: PATHS.src
+      },
+      {
+        test: /(\.css)$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }]
+  }
 };
