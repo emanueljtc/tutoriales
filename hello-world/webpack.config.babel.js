75b1943e20afe46d81b6e72496ccf8c3fb79075f
ema18787 <emanueljtc@gmail.com>
2018-07-23 16:46:58 -0400

    cambios

--- hello-world/webpack.config.babel.js
+++ hello-world/webpack.config.babel.js
@@ -1,6 +1,10 @@
 // Dependencies
 import webpack from 'webpack';
 import path from 'path';
+import ChunksPlugin from 'webpack-split-chunks';
+
+// Enviroment
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // Paths
 const PATHS = {
@@ -8,34 +12,69 @@ const PATHS = {
   build: path.join(__dirname, 'src/public'),
   src: path.join(__dirname, 'src')
 };
-export default {
-  devtool : 'cheap-module-eval-source-map',
-  entry : [
-    'webpack-hot-middleware/client?reload=true',
+
+const getDevTool = () => 'cheap-module-eval-source-map';
+const getEntry = () => {
+  const entry = [
     PATHS.index
-  ],
-  output: {
-    path: PATHS.build,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  module: {
-      loaders: [{
-        test: /\.js?$/,
-        loaders: ['babel-loader'],
-        include: PATHS.src
-      },
-      {
-        test: /(\.css)$/,
-        loaders: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-      }]
+  ];
+  if(isDevelopment) {
+    entry.push('webpack-hot-middleware/client?reload=true');
   }
+
+  return entry;
+};
+const getOutput = () => ({
+   path: PATHS.build,
+   publicPath: '/',
+   filename: '[name].bundle.js'
+});
+const getPlugins = () => {
+  const plugins = [
+    new ChunksPlugin({
+      to: 'vendor',
+      test: /node_modules/
+    })
+  ];
+  if(isDevelopment){
+    plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    );
+  }else {
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: false
+        }
+      })
+    );
+  }
+  return plugins;
+};
+const getLoaders = () => ({
+  loaders:
+  [
+    {
+      test: /\.js?$/,
+      loaders: ['babel-loader'],
+      include: PATHS.src
+    },
+    {
+      test: /(\.css)$/,
+      loaders: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    }
+  ]
+});
+export default {
+  devtool : getDevTool(),
+  entry : getEntry(),
+  output: getOutput(),
+  plugins: getPlugins(),
+  module: getLoaders()
 };
