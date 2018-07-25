797e4038178740f129cf2c11121f1e3195681df7
ema18787 <emanueljtc@gmail.com>
2018-07-25 16:22:24 -0400

    bug off-canvas

--- platzi-palooza/js/app.js
+++ platzi-palooza/js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function(ev) {
+    const panel = $(this).data('toggleDia');
+    console.log(panel)
+    $('lineup-tabs').foundation('selectTab', panel);
+})
