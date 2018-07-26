0fe79dff134c395f5a6b8f1a830cf136420e9a27
ema18787 <emanueljtc@gmail.com>
2018-07-26 16:35:35 -0400

    validations

---
+++ platzi-shoop/js/app.js
@@ -0,0 +1,23 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit((ev) => {
+    ev.preventDefault()
+
+    const error = Math.random() > 0.5
+    $formGroup.removeClass().addClass('form-group')
+    $formControl.removeClass().addClass('form-control')
+    $formGroup.addClass(error ? 'has-danger' : 'has-success')
+    $formControl.addClass(error ? 'form-control-danger' : 'form-control-success')
+    $formGroup.find('form-control-feedback').remove()
+    var el;
+    if (error) {
+        el = $('<div>', { html: '¡Upss ha ocurrido un error! <i class="fa fa-exclamation-triangle"></i>' })
+    } else {
+        el = $('<div>', { html: 'Te enviaremos todas las novedades. <i class="fa fa-check"></i>' })
+    }
+
+    el.addClass('form-control-feedback')
+    $formGroup.append(el)
+})
