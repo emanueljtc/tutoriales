d5f96493842b893f45d5781d2597dfcd76ea730f
ema18787 <emanueljtc@gmail.com>
2018-07-30 12:43:08 -0400

    importando el componente weatherLocation en App.js

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// Componentes
+import WeatherLocation from './components/WeatherLocation';
+// Assets
 import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Wheather Location
+        <WeatherLocation/>
       </div>
     );
   }
