0df4886a51147fa988649dbcbc1d777935f4813a
ema18787 <emanueljtc@gmail.com>
2018-07-31 15:00:17 -0400

    creando funcion getIconsName

--- weatherapp/src/components/WeatherTemperature.js
+++ weatherapp/src/components/WeatherTemperature.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
-const getWeatherIcon = weatherState => {
+const stateIconName = weatherState => {
   switch (weatherState) {
     case "cloud":
-      return (<WeatherIcons name="cloud" size="2x" />);
+      return "cloud";
+    case "cloudy":
+      return "cloudy";
+    case "sun":
+      return "day-sunny";
+    case "rain":
+      return "rain";
+    case "snow":
+      return "snow"
+    case "windy":
+      return "windy";
     default:
-      return (<WeatherIcons name="sleet" size="2x" />);
+      return "day-sunny";
   }
 };
+const getWeatherIcon = weatherState => {
+  return (<WeatherIcons name={stateIconName(weatherState)} size="2x" />);
+}
 const WeatherTemperature = ({temperature, weatherState}) => (
   <div>
   {getWeatherIcon(weatherState)}
