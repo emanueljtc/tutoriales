06975f64dbdaac224a29360cd5299f7666d1b35f
ema18787 <emanueljtc@gmail.com>
2018-08-01 13:40:12 -0400

    manejo de estados

--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -1,23 +1,48 @@
 //Dependencias
-import React from 'react';
+import React, { Component } from 'react';
 // Componentes
 import Location from './Location';
 import WeatherData from './WeatherData';
 // Constantes
-import { SUN } from './../../constants/weathers'
+import { SUN, WINDY } from './../../constants/weathers'
 // Estilo CSS
 import './styles.css';
-const data = {
+const data1 = {
+  temperature: 18,
+  weatherState: WINDY,
+  humidity: 5,
+  wind: '19 m/s'
+};
+const data2 = {
   temperature: 20,
   weatherState: SUN,
   humidity: 10,
   wind: '10 m/s'
 };
-const WheaterLocation = () => (
-  <div className='weatherLocationContainer'>
-    <Location city={'Villa de Cura'}/>
-    <WeatherData data={data}/>
-  </div>
-)
+class WheaterLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: 'Buenos Aires',
+      data: data1
+    };
+  }
+  handleUpdateClick = () => {
+    this.setState({
+      city: 'Buenos Aires',
+      data: data2
+    });
+    console.log('actualizado');
+  }
+ render = () => {
+    const { city, data } = this.state;
+    return (
+      <div className='weatherLocationContainer'>
+      <Location city={city}/>
+      <WeatherData data={data}/>
+      <button onClick={this.handleUpdateClick}>Actualizar</button>
+    </div>);
+  }
 
+}
 export default WheaterLocation;
