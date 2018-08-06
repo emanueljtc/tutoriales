db6de7e7c36c6e586253cf64f77328cf876ab537
ema18787 <emanueljtc@gmail.com>
2018-08-06 08:51:11 -0400

    aplicando grid

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col} from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // Componentes
 import LocationList from './components/LocationList';
@@ -21,10 +21,22 @@ class App extends Component {
     render() {
         return (
            <MuiThemeProvider>
-              <div className = "App">
-                <LocationList cities={cities}
-                  onSelectedLocation={this.handleSelectedLocation}/>
-              </div>
+              <Grid fluid>
+                <Row>
+                  <Col xs={12} sm={6} md={4} lg={3}>
+                    <div className="red"></div>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={3}>
+                    <div className='green'></div>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={3}>
+                    <div className='blue'></div>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={3}>
+                    <div className='green'></div>
+                  </Col>
+                </Row>
+              </Grid>
            </MuiThemeProvider>
         );
     }
