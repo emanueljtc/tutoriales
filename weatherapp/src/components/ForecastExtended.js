83928f2954b1e8c5bc11d082e3609d42f09ab2db
ema18787 <emanueljtc@gmail.com>
2018-08-07 11:16:50 -0400

    cambios state inicial

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 class ForecastExtended extends Component {
   render() {
     const { city } = this.props;
-    return (<div>Pronostico Extendido para {city}</div>);
+    return (
+      <div>
+        <h2>Pronóstico Extendido para {city}</h2>
+      </div>);
   }
 }
 
