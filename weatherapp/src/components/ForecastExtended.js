938ab313d9df91b2c9b8e934b3944b4c2225f459
ema18787 <emanueljtc@gmail.com>
2018-08-07 13:32:58 -0400

    componentDidMount

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 //Estilos CSS
 import './styles.css';
-
+/*
 const days = [
   "Lunes",
   "Martes",
@@ -17,17 +17,28 @@ const data = {
   humidity: 10,
   weatherState: 'normal',
   wind: 'normal',
-}
+}*/
 class ForecastExtended extends Component {
+  constructor() {
+    super();
+    this.state = { forecastData: null}
+  }
   renderForecastItemDays() {
-    return days.map( day => (<ForecastItem weekDay={day} hour={10} data={data}/>));
+    return "Render Items";
+    //return days.map( day => (<ForecastItem weekDay={day} hour={10} data={data}/>));
+  }
+  renderProgress = () => {
+     return <h3>Cargando Pronostico extendido...</h3>
   }
   render() {
     const { city } = this.props;
+    const { forecastData} = this.state;
     return (
       <div>
         <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
-        {this.renderForecastItemDays()}
+        { forecastData ?
+           this.renderForecastItemDays() :
+           this.renderProgress()}
       </div>);
   }
 }
