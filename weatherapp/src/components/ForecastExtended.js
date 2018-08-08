64c3aed6290821e1f783214788f0b11e5bb95b41
ema18787 <emanueljtc@gmail.com>
2018-08-08 09:58:28 -0400

    componentWillReceiveProps

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -29,7 +29,18 @@ class ForecastExtended extends Component {
   }
   componentDidMount() {
     // Fetch or axios
-    const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+    this.updateCity(this.props.city);
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.city !== this.props.city){
+      /* se establece el forecastData en null para que cargue de nuevo
+      progress */
+      this.setState({ forecastData: null});
+      this.updateCity(nextProps.city);
+    }
+  }
+  updateCity = city => {
+    const url_forecast = `${url}?q=${city}&appid=${api_key}`;
     fetch(url_forecast).then(
       data => (data.json())
     ).then(
