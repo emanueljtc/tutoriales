2f94cebc1fd8e9c06b5a0e8c814ffff853d0d91f
ema18787 <emanueljtc@gmail.com>
2018-08-10 10:43:14 -0400

    limpieza y cambio a presents component

--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -1,50 +1,28 @@
 //Dependencias
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CircularProgress from 'material-ui/CircularProgress';
 // Componentes
 import Location from './Location';
 import WeatherData from './WeatherData';
-import transformWeather from './../../services/transformWeather'
-
 
 // Estilo CSS
 import './styles.css';
-const api_key = "ba97e056eb1220e06c4a0b6aae37fd8b";
-const url = `http://api.openweathermap.org/data/2.5/weather`;
-class WheaterLocation extends Component {
-  constructor({ city }) {
-    super();
-    this.state = {
-      city,
-      data: null
-    };
-    console.log("constructor");
-  }
 
- componentWillMount() {
-   const { city } = this.state;
-   const api_weather = `${url}?q=${city}&appid=${api_key}`;
-   fetch(api_weather).then(data => {
-     return data.json();
-   }).then( weather_data => {
-     const data = transformWeather(weather_data);
-     this.setState({ data });
-   });
- }
-render = () => {
-    const { onWeatherLocationClick } = this.props;
-    const { city, data } = this.state;
-    return (
+const WheaterLocation = ({ onWeatherLocationClick, city, data }) => (
       <div className='weatherLocationContainer' onClick={onWeatherLocationClick}>
       <Location city={city}/>
       {data ? <WeatherData data={data}/> :  <CircularProgress size={60} thickness={7} /> }
-    </div>);
-  }
-
-}
+    </div>
+);
 WheaterLocation.propTypes = {
   city: PropTypes.string,
   onWeatherLocationClick: PropTypes.func,
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+  })
 }
 export default WheaterLocation;
