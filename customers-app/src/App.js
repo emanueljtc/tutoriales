43e5d564edeccd5c1818b6f8f1c02cd0888fa6ba
ema18787 <emanueljtc@gmail.com>
2018-08-14 17:30:53 -0400

    comentario

--- customers-app/src/App.js
+++ customers-app/src/App.js
@@ -1,6 +1,6 @@
+// Dependencias 
 import React, { Component } from 'react';
 import { BrowserRouter as Router , Route} from 'react-router-dom';
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
