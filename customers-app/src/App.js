8ae133e5d1ae01fe86aecb9e64b2d55ac263312b
ema18787 <emanueljtc@gmail.com>
2018-08-14 12:10:53 -0400

    aplicando browser Router

--- customers-app/src/App.js
+++ customers-app/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+          <div className="App">
+            <Link to="/customers">Customers</Link><br />
+            <Link to="/customers/30000000">Customers 30.000.000</Link>
+          </div>
+      </Router>
     );
   }
 }
