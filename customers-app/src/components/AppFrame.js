d964cde9de0e73247bc6f7ffe687bd981f2b2bce
ema18787 <emanueljtc@gmail.com>
2018-08-14 09:27:43 -0400

    nueva app

---
+++ customers-app/src/components/AppFrame.js
@@ -0,0 +1,21 @@
+// Dependencias
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppHeader from './AppHeader'
+
+const AppFrame = ({header, body}) => {
+   return (
+     <div>
+        <div class="app-frame">
+          <AppHeader title={props.header}></AppHeader>
+          <div>{body}</div>
+          <div>Aplicacion Simple de Ejemplo</div>
+        </div>
+     </div>
+   );
+};
+AppFrame.propTypes = {
+  header: PropTypes.string.isRequired,
+  body: PropTypes.element.isRequired,
+};
+export default AppFrame;
