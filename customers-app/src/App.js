47f0171368e8f049cf7ea6d674c8ab3d068bdbc5
ema18787 <emanueljtc@gmail.com>
2018-08-15 09:35:23 -0400

    implementacion de Switch

--- customers-app/src/App.js
+++ customers-app/src/App.js
@@ -1,25 +1,27 @@
 // Dependencias 
 import React, { Component } from 'react';
-import { BrowserRouter as Router , Route} from 'react-router-dom';
+import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
 
-  renderHome = () => <h1>Home</h1>;
+  renderHome = () => <h1>Inicio</h1>;
 
-  renderCustomerContainer = () => <h1>Customer Container</h1>;
+  renderCustomerContainer = () => <h2>Customer Container</h2>;
 
-  renderCustomerListContainer = () => <h1>Customer List Container</h1>;
+  renderCustomerListContainer = () => <h3>Customer List Container</h3>;
 
-  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+  renderCustomerNewContainer = () => <h4>Customer New Container</h4>;
   render() {
     return (
       <Router>
           <div>
             <Route exact path="/" component={this.renderHome} />
             <Route exact path="/customers" component={this.renderCustomerListContainer} />
-            <Route exact path="/customers/:dni" component={this.renderCustomerContainer} />
-            <Route exact path="/customers/new" component={this.renderCustomerNewContainer} />
+            <Switch>
+              <Route path="/customers/new" component={this.renderCustomerNewContainer} />
+              <Route path="/customers/:dni" component={this.renderCustomerContainer} />
+            </Switch>
           </div>
       </Router>
     );
