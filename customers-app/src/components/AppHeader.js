25554d73e3eab86ea857cacbb59b754d00bca988
ema18787 <emanueljtc@gmail.com>
2018-08-15 12:23:03 -0400

    fix bug AppHeader

--- customers-app/src/components/AppHeader.js
+++ customers-app/src/components/AppHeader.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AppHeader = props => {
-  return ( {
+  return ( 
     <div>
-        <div  className="app-header">
+        <div className="app-header">
           <h1>{props.title}</h1>
         </div>
     </div>
-  });
+  );
 };
 
 AppHeader.propTypes = {
