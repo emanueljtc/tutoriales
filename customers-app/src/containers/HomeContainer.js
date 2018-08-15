f600dcab751700de58ca49814d96f182810b2c66
ema18787 <emanueljtc@gmail.com>
2018-08-15 12:23:39 -0400

    Implementando history.push y withRouter

---
+++ customers-app/src/containers/HomeContainer.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import AppFrame from './../components/AppFrame';
+import CustomerActions from './../components/CustomersActions';
+
+class HomeContainer extends Component {
+  handleOnclick = () => {
+    console.log('Handle on Click')
+    this.props.history.push('/customers');
+  }
+
+  render() {
+    return (
+      <div>
+        <AppFrame
+          header='Home'
+          body={
+            <div>
+              esta es la pantalla inicial
+              <CustomerActions>
+                <button onClick={this.handleOnclick}>
+                  Listado de Clientes
+                </button>
+              </CustomerActions>
+            </div>
+          }
+        ></AppFrame>
+      </div>
+    );
+  }
+  
+};
+
+HomeContainer.propTypes = {
+  
+};
+
+export default withRouter(HomeContainer);
