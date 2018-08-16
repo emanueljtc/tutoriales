0cfe0504aab300410ad80ef0b5b997b99c1faed4
ema18787 <emanueljtc@gmail.com>
2018-08-16 15:08:20 -0400

    importando connect de react-redux

--- customers-app/src/containers/CustomersContainer.js
+++ customers-app/src/containers/CustomersContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import AppFrame from '../components/AppFrame';
 import CustomersList from '../components/CustomersList';
 import CustomersActions from '../components/CustomersActions';
+import { fetchCustomers } from '../actions/fetchCustomers';
 
 
 const customers = [
@@ -58,4 +60,9 @@ class CustomersContainers extends Component {
 
 }; */
 
-export default withRouter(CustomersContainers);
+const mapDispatchToProps = dispatch => ( 
+  {
+    fetchCustomers: () => dispatch(fetchCustomers())
+  }
+)
+export default withRouter(connect(null, mapDispatchToProps)(CustomersContainers));
