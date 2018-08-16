d72b966d92d2af4ad12e3bd96aab919887564ce7
ema18787 <emanueljtc@gmail.com>
2018-08-16 14:53:28 -0400

    vinculacion de provider

--- customers-app/src/index.js
+++ customers-app/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootComponent = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+ReactDOM.render(rootComponent, document.getElementById('root'));
 registerServiceWorker();
