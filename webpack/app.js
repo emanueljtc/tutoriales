659a51d17384cc71d80d63b1f1b9d15e95ef3209
ema18787 <emanueljtc@gmail.com>
2018-09-10 11:47:14 -0400

    webpack + jquery

--- webpack/app.js
+++ webpack/app.js
@@ -1,1 +1,7 @@
-alert('Hola Mundo.')
+var personas = require('./personas.js')
+var $ = require('jquery');
+
+$('body').append('<h1>'+'Nombre: '+personas[0].name+' '+'NickName: '+personas[0].nickname+'</h1>');
+$('body').append('<h1>' + 'Nombre: ' + personas[1].name + ' ' + 'NickName: ' + personas[1].nickname + '</h1>');
+
+console.log(personas);
---
+++ webpack/personas.js
@@ -0,0 +1,12 @@
+var personas = [
+  {
+    name: 'Jesus',
+    nickname: 'El Flaco'
+  },
+  {
+    name: 'Jose Andres',
+    nickname: 'El Zurdo'
+  }
+]
+
+module.exports = personas;
