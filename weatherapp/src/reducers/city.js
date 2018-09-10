e8ee8651930d6e8d6b457a7031a70289a8d2893e
ema18787 <emanueljtc@gmail.com>
2018-09-10 09:14:31 -0400

    cambios

--- weatherapp/src/reducers/city.js
+++
@@ -1,9 +0,0 @@
-import { SET_CITY } from './../actions';
-export const city = (state = {}, action) => { // Reducer
-  switch (action.type) {
-    case SET_CITY:
-      return action.payload; //Spread Operators ES&
-    default:
-        return state;
-  }
-}
